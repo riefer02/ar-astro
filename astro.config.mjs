ea1cd3fe3719b1b39dba96e9aefa235d5b3c371e
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2026-01-24 11:03:15 -0600

    feat: paginated blog search

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://andrewriefenstahl.com",
+  build: {
+    format: "directory",
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     sitemap(),
     react(),
+    pagefind(),
   ],
 });
